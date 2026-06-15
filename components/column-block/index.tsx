6e87a1667a13f22964c4b60827ba0a02f2b02650
bybyers <byers.jacob@gmail.com>
2026-06-15 15:21:55 -0700

    Update ColumnBlock component to improve layout responsiveness for larger screens by adjusting maximum width class.

--- components/column-block/index.tsx
+++ components/column-block/index.tsx
@@ -46,7 +46,7 @@ export default function ColumnBlock({
           )}
           {columns && Array.isArray(columns) && columns.length > 0 && (
             <div
-              className={`grid w-full gap-x-6 gap-y-5 lg:mx-auto lg:max-w-[75vw] ${gridCols}`}
+              className={`grid w-full gap-x-6 gap-y-5 lg:mx-auto 2xl:max-w-[75vw] ${gridCols}`}
             >
               {columns.map((column, index) => {
                 const isEmphasized =
